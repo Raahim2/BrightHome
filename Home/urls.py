5f3381741748ea352c69b58fbf8979126c4215ae
Raahim <127599591+Raahim2@users.noreply.github.com>
2024-01-31 17:36:02 +0530

    Update urls.py

--- Home/urls.py
+++ Home/urls.py
@@ -4,6 +4,9 @@ from Home import views
 
 urlpatterns = [
     path('', views.index, name='Home'),
+    path('UserLogin', views.UserLogin, name='UserLogin'),
+    path('SignUp', views.SignUp, name='SignUp'),
+    path('UserLogout', views.UserLogout, name='UserLogout'),
     path('cateogry/<str:cat>', views.cateogry, name='cateogry'),
     path('product/<int:mid>',views.product , name='product'),
     path('cart',views.cart ,name='cart' ),

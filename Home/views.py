cf9dede2b01f43e1bbee4d0f15f71c8f678b9721
Raahim <127599591+Raahim2@users.noreply.github.com>
2024-01-31 17:36:27 +0530

    Update views.py

--- Home/views.py
+++ Home/views.py
@@ -1,13 +1,57 @@
 from django.shortcuts import render , redirect
 from .models import Product
 from django.shortcuts import render
-from django.core.mail import send_mail
-from django.http import JsonResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import login , logout , authenticate
 from .models import Order
+
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def UserLogin(request):
+    messege=""
+    
+    
+    
+    if request.method=="POST":
+        username = request.POST.get("Username")
+        password = request.POST.get("Password")
+        
+        user = authenticate(username = username , password = password)
+
+        if user is not None:
+            login(request , user)
+            return redirect("/")
+        else :
+            messege="Incorrect Username or Password!"
+            return render(request,'login.html',{'messege': messege})
+        
+   
+        
+    return render(request,'login.html',{'messege': messege})
+    
+
+
+def UserLogout(request):
+    logout(request)
+    return render(request,'login.html')
+
+def SignUp(request):
+
+      if request.method=="POST":
+        username = request.POST.get("Username")
+        password = request.POST.get("Password")
+        print(username , password)
+
+        User.objects.create_user(username=username , password=password)
+        user = authenticate(request, username=username, password=password)
+        login(request, user)
+        return redirect("/")
+
+    
+      return render(request,'signup.html')
 
 def cateogry(request,cat):
     a=Product.objects.filter(cateogry=cat).values()
@@ -22,6 +66,9 @@ def cateogry(request,cat):
     return render(request , 'cateogry.html',context)
 
 def product(request , mid):
+    if request.user.is_anonymous:
+        return redirect("UserLogin")
+
     a=Product.objects.filter(id=mid).values()
     b = Product.objects.order_by('?')[:5].values('name','image','id')
     context={
@@ -31,16 +78,20 @@ def product(request , mid):
     return render(request, 'prodpage.html', context)
 
 def cart(request ):
+    if request.user.is_anonymous:
+        return redirect("UserLogin")
     return render(request,'cart.html')
 
-
-
 def checkout(request):
+    if request.user.is_anonymous:
+        return redirect("UserLogin")
+    name = request.user.username
+
     if request.method == 'POST':
+        
+
         form = Order(request.POST)
         if form:
-            
-            name = request.POST['name']
             email = request.POST['email']
             phone = request.POST['number']
             address = request.POST['address1']+" , "+request.POST['address2']+" , "+request.POST['address3']
@@ -56,8 +107,11 @@ def checkout(request):
     else:
         form = Order()
 
-    return render(request, 'checkout.html', {'form': form})
-
+    return render(request, 'checkout.html', {'username': name})
 
 def confirm(request):
+    if request.user.is_anonymous:
+        return redirect("UserLogin")
     return render(request,'confirm.html')
+
+
